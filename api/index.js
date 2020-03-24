051d2f23435ac48b25971e11b5c12c981cd90f4e
mriduava <mriduava@gmail.com>
2020-03-24 08:17:50 +0100

    create residencesInfo in db & access to api

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require('express'),
       app     = express(),
       sqlite  = require('sqlite3'),
+      path    = require('path'),
       util    = require('util');
 
 app.use(express.json());
 
 //Connect SQLite database
-const db = new sqlite.Database('../data/clearbnb.db')
+const dbPath = path.resolve(__dirname, '../data/clearbnb.db')
+const db = new sqlite.Database(dbPath)
 db.all = util.promisify(db.all)
 
 //Welcome message for test
@@ -18,9 +20,9 @@ app.get('/api/clearbnb', (req, res)=>{
 
 //Access residence data from ClearBnB DB
 app.get('/api/clearbnb/residences', async (req, res) => {
-  let residences = await db.all('SELECT * FROM residences')
+  let residences = await db.all(`SELECT * FROM residencesInfo`)
   res.json({
-    residences
+    residences: residences
   })
 })
 
@@ -30,7 +32,7 @@ app.get('/api/clearbnb/residences/:id', async (req, res) => {
     $id: req.params.id
   })
   res.json({
-    residence
+    residence: residence
   })
 })
 
