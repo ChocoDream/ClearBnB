79d3ecb36e3d74d0e413caa7f4c3bfafc097e549
mriduava <mriduava@gmail.com>
2020-03-24 07:11:39 +0100

    add databse & access data from db

--- api/index.js
+++ api/index.js
@@ -1,9 +1,36 @@
 const express = require('express'),
-      app     = express();
+      app     = express(),
+      sqlite  = require('sqlite3'),
+      util    = require('util');
 
+app.use(express.json());
+
+//Connect SQLite database
+const db = new sqlite.Database('../data/clearbnb.db')
+db.all = util.promisify(db.all)
+
+//Welcome message for test
 app.get('/api/clearbnb', (req, res)=>{
   res.json({
     welcome: 'Welcome to ClearBnB'
+  })
+})
+
+//Access residence data from ClearBnB DB
+app.get('/api/clearbnb/residences', async (req, res) => {
+  let residences = await db.all('SELECT * FROM residences')
+  res.json({
+    residences
+  })
+})
+
+//Access specific residence from ClearBnB DB
+app.get('/api/clearbnb/residences/:id', async (req, res) => {
+  let residence = await db.all('SELECT * FROM residences WHERE id = $id', {
+    $id: req.params.id
+  })
+  res.json({
+    residence
   })
 })
 
