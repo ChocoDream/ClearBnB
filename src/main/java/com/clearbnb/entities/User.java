79e5f72a3975ed57007430df6d247c0f9a61ef19
mriduava <mriduava@gmail.com>
2020-03-31 22:40:02 +0200

    add new input field & validation

--- src/main/java/com/clearbnb/entities/User.java
+++ src/main/java/com/clearbnb/entities/User.java
@@ -14,12 +14,18 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String email;
+    private String first_name;
+    private String last_name;
 
     public User() {}
 
-    public User(String username, String password) {
+    public User(String username, String password, String email, String first_name, String last_name) {
         this.username = username;
         this.password = password;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
     }
 
     public int getId() {
@@ -46,5 +52,29 @@ public class User {
     @JsonProperty
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
 }
--- src/main/resources/frontend/src/pages/Register.js
+++ src/main/resources/frontend/src/pages/Register.js
@@ -1,16 +1,25 @@
 import React, { useState, useContext } from 'react'
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, FormGroup, Label, Row, Col } from 'reactstrap';
+import { AvForm, AvField } from 'availity-reactstrap-validation';
 import { UserContext } from '../contexts/UserContextProvider'
 
 const Register = (props) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [first_name, setFirstName] = useState('')
+  const [last_name, setLastName] = useState('')
+  const [message, setMessage] = useState();
+
   const { setUser } = useContext(UserContext)
 
   const userRegister = async() => {
     const credentials = {
       username,
-      password
+      password,
+      email,
+      first_name,
+      last_name
     }
   
     let res = await fetch("/auth/register", {
@@ -23,35 +32,72 @@ const Register = (props) => {
       setUser(res)
       props.history.push('/')
     } catch {
-      console.log('Bad credentials');
+      setMessage('Alla fält är obligatoriska!')
     }
   }
 
   return (
     <div className="container">
-      <Form className="col-lg-6 col-sm-12 mx-auto">
+      <AvForm className="col-lg-6 col-sm-12 mx-auto">
         <h3 className="my-3 pb-3 text-info">Skapa konto</h3>
+        <Row className="row d-flex flex-row">
+          <Col className="col-lg-6">       
+          <FormGroup>
+            <Label for="firstName">Förnamn</Label>
+            <AvField 
+              value={first_name} 
+              onChange={e=>setFirstName(e.target.value)} 
+              type="text" name="firstName" 
+              id="firstName" 
+              required/>
+          </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="lastName">Efternamn</Label>
+              <AvField 
+                value={last_name} 
+                onChange={e=>setLastName(e.target.value)} 
+                type="text" name="lastName" 
+                id="lastName" 
+                required/>
+            </FormGroup>
+          </Col>
+         </Row>
+
         <FormGroup>
           <Label for="username">Username</Label>
-          <Input 
+          <AvField
             value={username} 
             onChange={e=>setUsername(e.target.value)} 
             type="username" 
             name="username" 
             id="username" 
-            placeholder="Username" />
+            required />
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
-          <Input 
+          <AvField
             value={password} 
             onChange={e=>setPassword(e.target.value)} 
             type="password" name="password" 
             id="password" 
-            placeholder="Password" />
+            required/>
         </FormGroup>
-         <Button color="info" onClick={userRegister}>Submit</Button>
-      </Form>
+        <FormGroup>
+          <Label for="email">E-post</Label>
+          <AvField
+            value={email} 
+            onChange={e=>setEmail(e.target.value)} 
+            type="email" name="email" 
+            id="email" 
+            required/>
+        </FormGroup>
+          <div className="d-flex flex-row">
+            <Button color="info" onClick={userRegister}>Submit</Button>
+            <div className="ml-4 pt-2 text-danger">{message}</div>
+          </div>
+      </AvForm>
     </div>
   )
 }
