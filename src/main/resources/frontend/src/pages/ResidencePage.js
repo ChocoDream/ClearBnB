83145d59b5ebad94495a3906f5a7e76904e4e91e
DanielHansson <dh99@live.se>
2020-04-04 04:12:17 +0200

    Added reading from Amenities by using AmenitiesList
    Going to ask tomorrow if I can modify the code so I can specify what amentities should be shown.

--- src/main/resources/frontend/src/pages/ResidencePage.js
+++ src/main/resources/frontend/src/pages/ResidencePage.js
@@ -3,22 +3,25 @@ import { ResidenceContext } from '../contexts/ResidenceContextProvider';
 import { Container, Button, Row, Col } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 
+import AmenityList from '../components/AmenitiesList';
+
 const ResidencePage = () => {
   let { id } = useParams();
   const { residence } = useContext(ResidenceContext);
-
 
   return (
     <Container>
       <Row>
         <Col>
           {/*BILD HÄR */}
-          <h2 className="mt-4">Fin villa i skåne trakterna</h2>
+          <h2 className="mt-4"> {residence.street_name}, {residence.city} </h2>
+          <h4 className="mt-2 text-muted"> {residence.region}, {residence.country} </h4>
           <Button color="primary" size="lg" block>Boka</Button>
           {/*ANVÄNDARE HÄR */}
           <h3 className="mt-3 mb-2"><b>{residence.price} kr</b> natt</h3>
-          <h2>Information on Boendet</h2>
-          
+          <h2>Information om boendet</h2>
+          <AmenityList />
+
         </Col>
       </Row>
     </Container>
