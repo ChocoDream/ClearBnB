e396338f5f297d72ab9bc8e112eb915388d0d9be
DanielHansson <dh99@live.se>
2020-04-06 18:19:50 +0200

    Workin on following a mockup

--- src/main/resources/frontend/src/pages/ResidencePage.js
+++ src/main/resources/frontend/src/pages/ResidencePage.js
@@ -9,21 +9,25 @@ const ResidencePage = () => {
   let { id } = useParams();
   const { residence, getResidence } = useContext(ResidenceContext);
 
+  useEffect(() => {
+    getResidence(id);
+  }, [])
+
+  
   return (
     <Container>
       <Row>
         <Col>
+          <h2 className="mt-2 h3"> <b>{residence.street_name} {residence.street_number}, {residence.city}</b> </h2>
+          <h4 className="mt-3 mb-3 text-muted"> {residence.region}, {residence.country} </h4>
           {/*BILD HÄR */}
-          <h2 className="mt-4"> {residence.street_name}, {residence.city} </h2>
-          <h4 className="mt-2 text-muted"> {residence.region}, {residence.country} </h4>
           <Link to="/create-booking">
-          <Button color="primary" size="lg" block>Boka</Button>
+          <Button color="info" size="lg" block>Boka</Button>
           </Link>
           {/*ANVÄNDARE HÄR */}
           <h3 className="mt-3 mb-2"><b>{residence.price} kr</b> natt</h3>
           <h2>Information om boendet</h2>
           <AmenityList />
-
         </Col>
       </Row>
     </Container>
