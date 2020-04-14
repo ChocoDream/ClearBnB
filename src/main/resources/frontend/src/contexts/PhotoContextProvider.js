4f0f99b2e6f3b9663b08ecd4ed5e7d0a486c3389
DanielHansson <dh99@live.se>
2020-04-14 14:45:28 +0200

    Adding photo to residenceInfo otw, added photo if no residencePic found
    It is a simple stock photo I found online. Right now it shows if the residence contains no photos, only got to hook up the photos from the db and call it a day.

--- src/main/resources/frontend/src/contexts/PhotoContextProvider.js
+++ src/main/resources/frontend/src/contexts/PhotoContextProvider.js
@@ -4,12 +4,24 @@ export const PhotoContext = createContext()
 
 export default function PhotoContextProvider(props) {
   const [photos, setPhotos] = useState([])
+  const [residencePhotos, setResidencePhotos] = useState([])
 
   //Get Data from API
   const getPhotos = async () => {
     let res = await fetch('/api/clearbnb/photos')
     res = await res.json()
     setPhotos(res)
+  }
+
+  const getPhotosByResidenceId = async (id) => {
+    let res = await fetch(`/api/clearbnb/photosByResidenceId/${id}`)
+    res = await res.json()
+    if (res.length === 0) { //If residence contains no photo
+      console.log("Is empty")
+      res = [{ path: "/assets/noavailablephoto.jpg"}] //HACKY WAY TO SHOW GIVE RESIDENCE WITHOUT A PHOTO AN UNAVAILBLE PHOTO
+    }
+    console.log(res)
+    setResidencePhotos(res)
   }
   
   const appendPhoto = (photo) => {
@@ -24,6 +36,9 @@ export default function PhotoContextProvider(props) {
     photos, 
     setPhotos,
     appendPhoto,
+    residencePhotos,
+    setResidencePhotos,
+    getPhotosByResidenceId
   }
 
   return (
--- src/main/resources/frontend/src/pages/ResidencePage.js
+++ src/main/resources/frontend/src/pages/ResidencePage.js
@@ -1,15 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ResidenceContext } from '../contexts/ResidenceContextProvider';
+import { PhotoContext } from '../contexts/PhotoContextProvider';
 import { Container, Row, Col } from 'reactstrap';
 import { useParams, Link } from 'react-router-dom';
+import PhotoCarousel from '../components/PhotoCarousel';
 
 const ResidencePage = () => {
   let { id } = useParams();
   const { residence, getResidenceById } = useContext(ResidenceContext);
-
+  const { getPhotosByResidenceId } = useContext(PhotoContext);
   const [totalPrice, setTotalPrice] = useState(Number);
 
   useEffect(() => {
+    getPhotosByResidenceId(id);
     getResidenceById(id);
   }, [])
   
@@ -22,24 +25,33 @@ const ResidencePage = () => {
   return (
     <Container>
       <Row>
-        {residence ? (<Col>
+        <Col>
           <h2 className="mt-2 h3"> <b>{residence.street_name} {residence.street_number}, {residence.city}</b> </h2>
           <h4 className="mt-3 mb-3 text-muted"> {residence.region}, {residence.country} </h4>
-          {/*BILD HÄR */}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <PhotoCarousel />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
           <span className="d-flex justify-content-center">
             <Link to="/create-booking" className="btn btn-info btn-lg">Boka</Link>
           </span>
-          {/*ANVÄNDARE HÄR */}
-          <div className="text-center">
             <h2 className="mt-2"><b>Totalt {totalPrice} kr </b></h2>
             <h3><b>{residence.price} kr</b> / natt</h3>
             <h3>2 personer</h3>
-            <h3>3 nätter</h3>
-          </div>
+          <h3>3 nätter</h3>
+        </Col>
+      </Row>
+      {/*ANVÄNDARE */}
+      <Row>
+        <Col>
           <h3><b>Information om boendet</b></h3>
           <p> logem lipsom logem lipson logem lipson logem lipson logem lipson logem lipson logen lipsom logem lipsom logen losipson lopgen lispon </p>
-        </Col>) : ''
-        }
+        </Col>
       </Row>
     </Container>
   )
