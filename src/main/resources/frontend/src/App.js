a642ddc40b9df4ef2765ac480fb4654b026fdc61
DanielHansson <dh99@live.se>
2020-04-16 23:58:50 +0200

    Reverted a change because something of a new update broke

--- src/main/resources/frontend/src/App.js
+++ src/main/resources/frontend/src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import MyPage from './pages/MyPage';
-import About from './pages/About';
-import Residences from './pages/Residences';
-import ResidencePage from './pages/ResidencePage';
-import NewResidence from './pages/NewResidence';
-import CreateBooking from './pages/CreateBooking';
-  
-import NavMenu from './components/NavMenu';
-import Footer from './components/Footer';
-import ResidenceContextProvider from './contexts/ResidenceContextProvider'
-import ResidenceConProvider from './contexts/ResidenceConProvider'
-import AddressContextProvider from './contexts/AddressContextProvider'
-import UserContextProvider from './contexts/UserContextProvider'
-import CityContextProvider from './contexts/CityContextProvider'
-import AmenityContextProvider from './contexts/AmenityContextProvider'
-import AmenitiesResidencesIdConProvider from './contexts/AmenitiesResidencesIdConProvider'
-import BookingContextProvider from './contexts/BookingContextProvider'
-import PhotoContextProvider from './contexts/PhotoContextProvider'
-import OwnersResidencesIdConProvider from './contexts/OwnersResidencesIdConProvider'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import MyPage from "./pages/MyPage";
+import About from "./pages/About";
+import Residences from "./pages/Residences";
+import ResidencePage from "./pages/ResidencePage";
+import NewResidence from "./pages/NewResidence";
+import CreateBooking from "./pages/CreateBooking";
+
+import NavMenu from "./components/NavMenu";
+import Footer from "./components/Footer";
+
+import ResidenceContextProvider from "./contexts/ResidenceContextProvider";
+import ResidenceConProvider from "./contexts/ResidenceConProvider";
+import AddressContextProvider from "./contexts/AddressContextProvider";
+import UserContextProvider from "./contexts/UserContextProvider";
+import CityContextProvider from "./contexts/CityContextProvider";
+import AmenityContextProvider from "./contexts/AmenityContextProvider";
+import AmenitiesResidencesIdConProvider from "./contexts/AmenitiesResidencesIdConProvider";
+import BookingContextProvider from "./contexts/BookingContextProvider";
+import PhotoContextProvider from "./contexts/PhotoContextProvider";
+import OwnersResidencesIdConProvider from "./contexts/OwnersResidencesIdConProvider";
 
 const App = () => {
   return (
@@ -42,17 +40,16 @@ const App = () => {
                         <Router>
                           <NavMenu />
                             <div className="content">
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/about" component={About} />
-                            <Route exact path="/user-login" component={Login} />
-                            <Route exact path="/user-register" component={Register} />
-                            <Route exact path="/mypage" component={MyPage} />
-                            <Route exact path="/residences" component={Residences} />
-                            <Route exact path="/new-residence" component={NewResidence} />
-                            <Route exact path="/create-booking" component={CreateBooking} />
-                            <Route exact path="/residences/:id" children={<ResidencePage />} />
+                              <Route exact path="/" component={Home} />
+                              <Route exact path="/about" component={About} />
+                              <Route exact path="/user-login" component={Login} />
+                              <Route exact path="/user-register" component={Register} />
+                              <Route exact path="/mypage" component={MyPage} />
+                              <Route exact path="/residences" component={Residences} />
+                              <Route exact path="/new-residence" component={NewResidence} />
+                              <Route exact path="/create-booking" component={CreateBooking} />
+                              <Route exact path="/residences/:id" children={<ResidencePage />} />
                             </div>
-                          <Footer />
                         </Router>
                       </OwnersResidencesIdConProvider>
                     </PhotoContextProvider>
--- src/main/resources/frontend/src/components/Footer.js
+++ src/main/resources/frontend/src/components/Footer.js
@@ -1,10 +1,59 @@
-import React, { useState, useContext } from 'react';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
 
-const Footer = () => (
-    <footer className="footer">
-      <p>ClearBnB</p>
-    </footer>
+const Footer = () => {
+  return (
+    <Container
+      className="mt-5"
+      
+    >
+      <Row className="font-small indigo " >
+        <Col className="mb-3">
+          <h6 className="text-uppercase font-weight-bold">
+            <a href="#!">Residences</a>
+          </h6>
+        </Col>
+        <Col className="mb-3">
+          <h6 className="text-uppercase font-weight-bold">
+            <a href="#!">Om oss</a>
+          </h6>
+        </Col>
+        <Col className="mb-3">
+          <h6 className="text-uppercase font-weight-bold">
+            <a href="#!">Logga in</a>
+          </h6>
+        </Col>
+
+        <Col className="mb-3">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="#!">Skapa konto</a>
+          </h6>
+        </Col>
+      </Row>
+
+      <hr className="rgba-white-light" />
+      <Row>
+        <Col
+          md="12"
+          className="mt-3 d-flex text-center justify-content-center mb-md-0 mb-4"
+        >
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+          blanditiis velit quod incidunt et non iste animi facilis illo ratione
+          porro unde consectetur, repudiandae veniam aut, mollitia, neque rerum
+          quae.
+        </Col>
+      </Row>
+
+      <hr
+        className="clearfix d-md-none rgba-white-light"
+        style={{ margin: "10% 15% 5%" }}
+      />
+
+      <Row className="text-center py-3">
+        <Col md="12">© 2020 Clear BnB:</Col>
+      </Row>
+    </Container>
   );
-
+};
 
 export default Footer;
