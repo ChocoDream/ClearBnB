6316f3c4e61c187cf5d6066eee26f1cb3ff9a853
mriduava <mriduava@gmail.com>
2020-04-17 09:31:36 +0200

    modify footer & add link

--- src/main/resources/frontend/src/components/Footer.js
+++ src/main/resources/frontend/src/components/Footer.js
@@ -1,56 +1,54 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import { Container, Row, Col } from "reactstrap";
 
 const Footer = () => {
   return (
-    <Container
-      className="mt-5"
-      
-    >
-      <Row className="font-small indigo " >
-        <Col className="mb-3">
-          <h6 className="text-uppercase font-weight-bold">
-            <a href="#!">Residences</a>
-          </h6>
-        </Col>
-        <Col className="mb-3">
-          <h6 className="text-uppercase font-weight-bold">
-            <a href="#!">Om oss</a>
-          </h6>
-        </Col>
-        <Col className="mb-3">
-          <h6 className="text-uppercase font-weight-bold">
-            <a href="#!">Logga in</a>
-          </h6>
-        </Col>
-
-        <Col className="mb-3">
-          <h6 class="text-uppercase font-weight-bold">
-            <a href="#!">Skapa konto</a>
-          </h6>
-        </Col>
-      </Row>
-
-      <hr className="rgba-white-light" />
-      <Row>
-        <Col
-          md="12"
-          className="mt-3 d-flex text-center justify-content-center mb-md-0 mb-4"
-        >
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
-          blanditiis velit quod incidunt et non iste animi facilis illo ratione
-          porro unde consectetur, repudiandae veniam aut, mollitia, neque rerum
-          quae.
-        </Col>
-      </Row>
-
-      <hr
-        className="clearfix d-md-none rgba-white-light"
-        style={{ margin: "10% 15% 5%" }}
-      />
-
-      <Row className="text-center py-3">
-        <Col md="12">© 2020 Clear BnB:</Col>
+    <Container fluid className="bg-light border-top border-info mt-5">
+      <Container
+        className="mt-5 mb-3"      
+      >
+        <Row className="font-small indigo " >
+          <Col className="mb-3">
+            <h6 className="text-uppercase font-weight-bold text-center">
+              <Link to="/">Bostäder</Link>
+            </h6>
+          </Col>
+          <Col className="mb-3">
+            <h6 className="text-uppercase font-weight-bold text-center">
+              <Link to="/about">Om oss</Link>
+            </h6>
+          </Col>
+          <Col className="mb-3">
+            <h6 className="text-uppercase font-weight-bold text-center">
+              <Link to="/user-login">Logga in</Link>
+            </h6>
+          </Col>
+          <Col className="mb-3">
+            <h6 className="text-uppercase font-weight-bold text-center text-info">
+              <Link to="/user-register">Skapa konto</Link>
+            </h6>
+          </Col>
+        </Row>
+        <hr className="rgba-white-light" />
+        <Row>
+          <Col
+            md="12"
+            className="my-3 d-flex text-center justify-content-center mb-md-0 mb-4"
+          >
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum
+            blanditiis velit quod incidunt et non iste animi facilis illo ratione
+            porro unde consectetur, repudiandae veniam aut, mollitia, neque rerum
+            quae.
+          </Col>
+        </Row>
+        <hr
+          className="clearfix d-md-none rgba-white-light"
+          style={{ margin: "10% 15% 5%" }}
+        />
+      </Container>
+      <Row className="text-center py-3 bg-info text-white mt-3">
+          <Col md="12">© 2020 Clear BnB</Col>
       </Row>
     </Container>
   );
